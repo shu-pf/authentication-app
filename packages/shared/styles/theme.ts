53adadb068cea791b0b5e393b501d9ba21321ca1
shu-pf <s_furuse@yumemi.co.jp>
2022-07-21 01:28:27 +0900

    themeを追加

---
+++ packages/shared/styles/theme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  colors: {
+    // このUIで基調となるメインカラー、塗りやテキストに使います。
+    mainPrimary: "#2F80ED",
+    // 塗りやテキストに使います。
+    basePrimary: "#000000",
+    baseSecondary: "#333333",
+    baseTertiary: "#9E9E9E",
+    baseQuaternary: "#828282",
+    baseLink: "#2D9CDB",
+    // 背景色に使います。
+    bgPrimary: "#FFFFFF",
+    // 区切り線や囲み線に使います。
+    separateContents: "#BDBDBD",
+    // エラーや成功などを知らせる時使います。（マテリアルカラー表参考）
+    information: {
+      //
+    },
+    // 背景やオブジェクトに重ねる色として使います。
+    overlay: {
+      //
+    },
+    // イレギュラーなケースで、特定の機能やページで使われる色です。
+    unique: {
+      //
+    },
+  },
+};
