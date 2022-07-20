d87ec409fdce1436d31061a4b95664398059e09f
shu-pf <s_furuse@yumemi.co.jp>
2022-07-21 00:43:33 +0900

    ressの導入

--- packages/shared/.storybook/preview.js
+++ packages/shared/.storybook/preview.js
@@ -1,3 +1,6 @@
+// リセットCSS
+import "ress";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +9,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
