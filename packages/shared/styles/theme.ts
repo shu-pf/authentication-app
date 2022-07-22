dfb96ecf3fc9373aadbbd06a8d9ddeeeac44f16c
shu-pf <s_furuse@yumemi.co.jp>
2022-07-23 07:16:20 +0900

    typographiesの追加

--- packages/shared/styles/theme.ts
+++ packages/shared/styles/theme.ts
@@ -28,4 +28,96 @@ export const theme: Theme = {
     //   //
     // },
   },
+  typographies: {
+    h1: {
+      fontWeight: 400,
+      fontSize: "36px",
+      lineHeight: "49.03px",
+      letterSpacing: "-3.5%",
+    },
+    h2: {
+      fontWeight: 400,
+      fontSize: "24px",
+      lineHeight: "32.69px",
+      letterSpacing: "-3.5%",
+    },
+    h3: {
+      fontWeight: 600,
+      fontSize: "18px",
+      lineHeight: "24.52px",
+      letterSpacing: "-3.5%",
+    },
+    buttonBold: {
+      fontWeight: 600,
+      fontSize: "16px",
+      lineHeight: "21.79px",
+      letterSpacing: "-3.5%",
+    },
+    buttonRegular: {
+      fontWeight: 500,
+      fontSize: "16px",
+      lineHeight: "21.79px",
+      letterSpacing: "-3.5%",
+    },
+    subTitle: {
+      fontWeight: 300,
+      fontSize: "18px",
+      lineHeight: "25px",
+      letterSpacing: "-3.5%",
+    },
+    input: {
+      fontWeight: 400,
+      fontSize: "16px",
+      lineHeight: "21.79px",
+      letterSpacing: "-3.5%",
+    },
+    label: {
+      fontWeight: 500,
+      fontSize: "13px",
+      lineHeight: "17.71px",
+      letterSpacing: "-3.5%",
+    },
+    body: {
+      fontWeight: 400,
+      fontSize: "16px",
+      lineHeight: "21.79px",
+      letterSpacing: "-3.5%",
+    },
+    caption: {
+      fontWeight: 400,
+      fontSize: "14px",
+      lineHeight: "19px",
+      letterSpacing: "-3.5%",
+    },
+    tableCaption: {
+      fontWeight: 500,
+      fontSize: "13px",
+      lineHeight: "17.71px",
+      letterSpacing: "-3.5%",
+    },
+    tableBody: {
+      fontWeight: 500,
+      fontSize: "18px",
+      lineHeight: "24.52px",
+      letterSpacing: "-3.5%",
+    },
+    link: {
+      fontWeight: 400,
+      fontSize: "14px",
+      lineHeight: "-3.5%",
+      letterSpacing: "-3.5%",
+    },
+    footerBold: {
+      fontWeight: 600,
+      fontSize: "14px",
+      lineHeight: "19px",
+      letterSpacing: "-3.5%",
+    },
+    footerRegular: {
+      fontWeight: 400,
+      fontSize: "14px",
+      lineHeight: "19px",
+      letterSpacing: "-3.5%",
+    },
+  },
 };
--- packages/shared/types/emotion.d.ts
+++ packages/shared/types/emotion.d.ts
@@ -1,5 +1,12 @@
 import "@emotion/react";
 
+interface Typography {
+  fontWeight: number;
+  fontSize: string;
+  lineHeight: string;
+  letterSpacing: string;
+}
+
 declare module "@emotion/react" {
   export interface Theme {
     colors: {
@@ -12,6 +19,23 @@ declare module "@emotion/react" {
       baseLink: string;
       bgPrimary: string;
       separateContents: string;
+    };
+    typographies: {
+      h1: Typography;
+      h2: Typography;
+      h3: Typography;
+      buttonBold: Typography;
+      buttonRegular: Typography;
+      input: Typography;
+      label: Typography;
+      subTitle: Typography;
+      body: Typography;
+      caption: Typography;
+      tableCaption: Typography;
+      tableBody: Typography;
+      link: Typography;
+      footerBold: Typography;
+      footerRegular: Typography;
     };
   }
 }
