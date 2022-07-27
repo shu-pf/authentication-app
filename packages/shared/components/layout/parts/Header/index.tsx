3ac520f05ffb03447c6359a6d33f97a9a7797e52
shu-pf <s_furuse@yumemi.co.jp>
2022-07-27 12:07:10 +0900

    logo コンポーネントの設置

--- packages/shared/components/layout/parts/Header/index.tsx
+++ packages/shared/components/layout/parts/Header/index.tsx
@@ -1,11 +1,10 @@
+import { Logo } from "../../../ui/Logo";
 import * as Styled from "./index.style";
 
 export const Header: React.FC = () => {
   return (
     <Styled.Content>
-      <Styled.Regular>
-        created by <Styled.Bold>username</Styled.Bold>
-      </Styled.Regular>
+      <Logo />
       <Styled.Regular>devChallenges.io</Styled.Regular>
     </Styled.Content>
   );
