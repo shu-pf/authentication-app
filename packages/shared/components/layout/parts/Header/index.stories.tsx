732ef785c688c9550c792449ecf20922c2379a44
shu-pf <s_furuse@yumemi.co.jp>
2022-07-31 02:02:16 +0900

    ヘッダーに埋め込み

--- packages/shared/components/layout/parts/Header/index.stories.tsx
+++ packages/shared/components/layout/parts/Header/index.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Header as Component } from ".";
+import { DropDownNavigation } from "../../../ui/DropDownNavigation/index.stories";
 
 export default {
   component: Component,
@@ -12,3 +13,7 @@ const Template: ComponentStory<typeof Component> = (args) => (
 );
 
 export const Header = Template.bind({});
+Header.args = {
+  // @ts-ignore argsに値を代入しているので未定義になるはずはないが、未定義と定義されてエラーになっている
+  navigation: <DropDownNavigation {...DropDownNavigation.args} />,
+};
