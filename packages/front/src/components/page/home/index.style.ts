b36970522ee14bf8b6093057fc07883af0eaa09a
shu-pf <s_furuse@yumemi.co.jp>
2022-08-05 18:40:04 +0900

    Personal infoページの追加

--- packages/front/src/components/page/home/index.style.ts
+++ packages/front/src/components/page/home/index.style.ts
@@ -1,6 +1,31 @@
 import styled from "@emotion/styled";
 
+export const TitleContainer = styled.div({
+  padding: "28px 49px",
+});
+
 export const Title = styled.h1(({ theme }) => ({
   color: theme.colors.basePrimary,
   ...theme.typographies.h2,
+}));
+
+export const SubTitle = styled.p(({ theme }) => ({
+  color: theme.colors.baseQuaternary,
+  ...theme.typographies.tableCaption,
+}));
+
+export const Row = styled.div(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: "24px 50px",
+}));
+
+export const RowTitle = styled.div(({ theme }) => ({
+  ...theme.typographies.tableCaption,
+  color: theme.colors.separateContents,
+  width: "231px",
+}));
+
+export const RowContent = styled.div(({ theme }) => ({
+  color: theme.colors.baseSecondary,
 }));
--- packages/front/src/components/page/home/index.tsx
+++ packages/front/src/components/page/home/index.tsx
@@ -1,9 +1,50 @@
-import { Title } from "./index.style";
+import { Icon } from "shared/components/ui";
+import { Divider } from "shared/components/util";
+import * as Styled from "./index.style";
 
 export const HomeContainer: React.FC = () => {
   return (
     <>
-      <Title>Profile</Title>
+      <Styled.TitleContainer>
+        <Styled.Title>Profile</Styled.Title>
+        <Styled.SubTitle>
+          Some info may be visible to other people
+        </Styled.SubTitle>
+      </Styled.TitleContainer>
+      <Divider />
+      <Styled.Row>
+        <Styled.RowTitle>PHOTO</Styled.RowTitle>
+        <Styled.RowContent>
+          <Icon name="UserCircle" size="72px"></Icon>
+        </Styled.RowContent>
+      </Styled.Row>
+      <Divider />
+      <Styled.Row>
+        <Styled.RowTitle>NAME</Styled.RowTitle>
+        <Styled.RowContent>Xanthe Neal</Styled.RowContent>
+      </Styled.Row>
+      <Divider />
+      <Styled.Row>
+        <Styled.RowTitle>BIO</Styled.RowTitle>
+        <Styled.RowContent>
+          I am a software developer and a big fan of devchallenges...
+        </Styled.RowContent>
+      </Styled.Row>
+      <Divider />
+      <Styled.Row>
+        <Styled.RowTitle>PHONE</Styled.RowTitle>
+        <Styled.RowContent>908249274292</Styled.RowContent>
+      </Styled.Row>
+      <Divider />
+      <Styled.Row>
+        <Styled.RowTitle>EMAIL</Styled.RowTitle>
+        <Styled.RowContent>xanthe.neal@gmail.com</Styled.RowContent>
+      </Styled.Row>
+      <Divider />
+      <Styled.Row>
+        <Styled.RowTitle>PASSWORD</Styled.RowTitle>
+        <Styled.RowContent>************</Styled.RowContent>
+      </Styled.Row>
     </>
   );
 };
