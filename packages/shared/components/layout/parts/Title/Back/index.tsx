e66d675c52de28cccf937745547b2095f9604a1c
shu-pf <s_furuse@yumemi.co.jp>
2022-08-08 11:12:22 +0900

    戻るボタンを実装

--- packages/shared/components/layout/parts/Title/Back/index.tsx
+++ packages/shared/components/layout/parts/Title/Back/index.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from "next/router";
+import { MouseEventHandler } from "react";
 import { TextButton } from "../../../../ui";
 import { Spacer } from "../../../../util";
 import * as Styled from "./index.style";
@@ -6,10 +8,18 @@ import * as Styled from "./index.style";
  * 見出しレイアウトです。戻るボタンを表示します。
  */
 export const BackTitle: React.FC = () => {
+  const router = useRouter();
+
+  const handleClick: MouseEventHandler = () => {
+    router.back();
+  };
+
   return (
     <Styled.TitleContainer>
       <Spacer height="48px" />
-      <TextButton iconName="AngleLeft">Back</TextButton>
+      <TextButton iconName="AngleLeft" onClick={handleClick}>
+        Back
+      </TextButton>
       <Spacer height="30px" />
     </Styled.TitleContainer>
   );
