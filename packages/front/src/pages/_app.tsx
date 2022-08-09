875bd4fce4e881df5fed07fae1a9da23cc69f47f
shu-pf <s_furuse@yumemi.co.jp>
2022-08-09 11:35:18 +0900

    auth 0 を導入

--- packages/front/src/pages/_app.tsx
+++ packages/front/src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "shared/styles/globals";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
+import { Auth0Provider } from "@auth0/auth0-react";
 // リセットCSS
 import "ress";
 
@@ -20,10 +21,16 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Global styles={GlobalStyle} />
-      {getLayout(<Component {...pageProps} />)}
-    </ThemeProvider>
+    <Auth0Provider
+      domain="dev-mkemjxp8.us.auth0.com"
+      clientId="APoDm1UK46dJwrBWeVeSEZqTf8KhKKK0"
+      redirectUri={window.location.origin}
+    >
+      <ThemeProvider theme={theme}>
+        <Global styles={GlobalStyle} />
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+    </Auth0Provider>
   );
 }
 
