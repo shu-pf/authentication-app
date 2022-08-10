3c5350fff43ceb2a5a071abbc4c6fcb65d49c046
shu-pf <s_furuse@yumemi.co.jp>
2022-08-11 00:53:36 +0900

    レイアウトを変更

--- packages/front/src/pages/_app.tsx
+++ packages/front/src/pages/_app.tsx
@@ -2,24 +2,11 @@ import { ThemeProvider, Global } from "@emotion/react";
 import { theme } from "shared/styles/theme";
 import { GlobalStyle } from "shared/styles/globals";
 import type { AppProps } from "next/app";
-import type { ReactElement, ReactNode } from "react";
-import type { NextPage } from "next";
 import { Auth0Provider } from "@auth0/auth0-react";
 // リセットCSS
 import "ress";
 
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  // ページレベルで定義されたレイアウトがある場合はそれを使用する
-  const getLayout = Component.getLayout ?? ((page) => page);
-
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Auth0Provider
       domain="devchallenges.jp.auth0.com"
@@ -28,7 +15,7 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     >
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        {getLayout(<Component {...pageProps} />)}
+        {<Component {...pageProps} />}
       </ThemeProvider>
     </Auth0Provider>
   );
