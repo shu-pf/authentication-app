f749d4884fa0538439a43487118a71ba75698314
shu-pf <s_furuse@yumemi.co.jp>
2022-09-12 02:38:17 +0900

    apolloの削除

--- packages/front/src/libs/apollo-client.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-// Apolloクライアントの作成
-export const client = new ApolloClient({
-  uri: "https://asia-northeast1-steady-ether-356216.cloudfunctions.net/authentication-app",
-  cache: new InMemoryCache(),
-});
